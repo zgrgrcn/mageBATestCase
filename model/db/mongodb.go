6d1bf6eacb75f560a9ea4a91a189c8315e8503ed
zgrgrcn <22799339+zgrgrcn@users.noreply.github.com>
2022-08-23 12:51:10 +0300

    mongo connection model

---
+++ model/db/mongodb.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"mageBATestCase/util"
+	"time"
+)
+
+var client *mongo.Client = nil
+var ctx context.Context = nil
+
+func GetConnection() (*mongo.Client, context.Context) {
+	if client == nil {
+		client, err := mongo.NewClient(options.Client().ApplyURI(util.GetEnvVariable("ATLAS_URI")))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = client.Ping(ctx, readpref.Primary())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Connected to MongoDB!")
+		return client, ctx
+	}
+
+	return client, ctx
+}
